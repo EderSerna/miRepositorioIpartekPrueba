a313d710534d64a2891a6a8dbbd36d75b8c30853
EderSerna <serna.eder74@gmail.com>
2019-06-17 08:50:14 +0200

    Falla pls

--- src/com/ipartek/formacion/BigTest.java
+++ src/com/ipartek/formacion/BigTest.java
@@ -5,7 +5,7 @@ import java.math.BigInteger;
 public class BigTest {
 	public static void main(String[] args) {
 	   
-	    	//Mira que numeros mas...Grandes 7u7
+	    	//Mira que numeros mas... Grandes 7u7
 	        // Initialize result 
 	        BigInteger f = new BigInteger("1"); // Or BigInteger.ONE 
 	  
