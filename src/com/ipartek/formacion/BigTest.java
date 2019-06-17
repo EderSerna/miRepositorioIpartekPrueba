4a75b63ff1c05563eb35fee121cefc390e4695f3
EderSerna <serna.eder74@gmail.com>
2019-06-17 08:35:51 +0200

    Mira como falla

--- src/com/ipartek/formacion/BigTest.java
+++ src/com/ipartek/formacion/BigTest.java
@@ -5,10 +5,11 @@ import java.math.BigInteger;
 public class BigTest {
 	public static void main(String[] args) {
 	   
-	    
+	    	//Mira que numeros mas...Grandes 7u7
 	        // Initialize result 
 	        BigInteger f = new BigInteger("1"); // Or BigInteger.ONE 
 	  
+	        
 	        // Multiply f with 2, 3, ...N 
 	        for (int i = 2; i <= 1000; i++) 
 	            f = f.multiply(BigInteger.valueOf(i)); 
